394a193b0a78853bf7380bad7023826f0c8c1105
Hoorad <hoorad.abootalebi@gmail.com>
2021-11-08 17:58:31 -0500

    Send integer from Zumo to ESP to Server

---
+++ Zumo/serial.ino
@@ -0,0 +1,43 @@
+/*
+  Software serial multple serial test
+
+ Receives from the hardware serial, sends to software serial.
+ Receives from software serial, sends to hardware serial.
+
+ The circuit:
+ * RX is digital pin 2 (connect to TX of other device)
+ * TX is digital pin 3 (connect to RX of other device)
+
+ Note:
+ Not all pins on the Mega and Mega 2560 support change interrupts,
+ so only the following can be used for RX:
+ 10, 11, 12, 13, 50, 51, 52, 53, 62, 63, 64, 65, 66, 67, 68, 69
+
+ Not all pins on the Leonardo support change interrupts,
+ so only the following can be used for RX:
+ 8, 9, 10, 11, 14 (MISO), 15 (SCK), 16 (MOSI).
+
+ created back in the mists of time
+ modified 25 May 2012
+ by Tom Igoe
+ based on Mikal Hart's example
+
+ This example code is in the public domain.
+
+ */
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(13, 14); // RX, TX
+
+char mystr[7] = "H"; //String data
+
+void setup() {
+  // Begin the Serial at 9600 Baud
+  mySerial.begin(9600);
+  
+}
+
+void loop() {
+  mySerial.write("h"); //Write the serial data
+  delay(1000);
+}
